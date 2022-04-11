a9c9375be8d7b373c5af593c73dc51be7144b19e
Richard <iwannaplaygamesjp@gmail.com>
2022-04-11 21:30:06 +0800

    finished init setup

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,8 @@ class Boot extends Phaser.Scene {
 }
 
 const game = new Phaser.Game({
-    width: 800,
-    height: 600,
+    width: 640,
+    height: 360,
     backgroundColor: "#2f2f2f",
     scale: {
         mode: Phaser.Scale.ScaleModes.FIT,
--- src/scenes/Level.ts
+++ src/scenes/Level.ts
@@ -3,6 +3,8 @@
 /* START OF COMPILED CODE */
 
 import Phaser from "phaser";
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
 
 export default class Level extends Phaser.Scene {
 
@@ -16,14 +18,29 @@ export default class Level extends Phaser.Scene {
 
 	editorCreate(): void {
 
+		// platformer_fun
+		const platformer_fun = this.add.tilemap("platformer_fun");
+		platformer_fun.addTilesetImage("pixelPlatformer", "tiles_packed");
+
 		// text
-		const text = this.add.text(400, 436, "", {});
+		const text = this.add.text(456, 58, "", {});
 		text.setOrigin(0.5, 0.5);
-		text.text = "Phaser 3 + Phaser Editor 2D\nVite + TypeScript";
+		text.text = "Gamedev JS Game Jam\n2022";
 		text.setStyle({ "align": "center", "fontFamily": "Arial", "fontSize": "3em" });
 
 		// image
-		this.add.image(400, 243, "FufuSuperDino");
+		this.add.image(132, 86, "gamedevjs");
+
+		// ground_1
+		platformer_fun.createLayer("ground", ["pixelPlatformer"], 0, 0);
+
+		// block_1
+		platformer_fun.createLayer("block", ["pixelPlatformer"], 0, 0);
+
+		// water_1
+		platformer_fun.createLayer("water", ["pixelPlatformer"], 0, 0);
+
+		this.platformer_fun = platformer_fun;
 
 		this.events.emit("scene-awake");
 	}
