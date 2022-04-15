702293bfe3c5f0c563f3ea68d7a0e0c7e180c799
Richard <iwannaplaygamesjp@gmail.com>
2022-04-15 17:43:18 +0800

    player can move with input

--- src/scenes/Level.ts
+++ src/scenes/Level.ts
@@ -4,8 +4,10 @@
 
 import Phaser from "phaser";
 import TileMapLayerPhysics from "../components/TileMapLayerPhysics";
-import Movement from "../components/Movement";
 import Physics from "../components/Physics";
+import KeyboardInput from "../components/KeyboardInput";
+import JustMovement from "../components/JustMovement";
+import Animation from "../components/Animation";
 /* START-USER-IMPORTS */
 /* END-USER-IMPORTS */
 
@@ -32,23 +34,45 @@ export default class Level extends Phaser.Scene {
 		const wall_1 = cave_test_map_1.createLayer("wall", ["Gamdev jam cate tiles test 1"], 0, 0);
 
 		// player
-		const player = this.add.sprite(158, 144, "swarm-right-walk-1");
+		const player = this.add.sprite(160, 175, "dude-front-walk-1");
+
+		// enemy
+		const enemy = this.add.sprite(136, 71, "swarm-front-walk-1");
+
+		// lists
+		const enemyTeam = [enemy];
 
 		// wall_1 (components)
 		new TileMapLayerPhysics(wall_1);
 
 		// player (components)
-		new Movement(player);
 		const playerPhysics = new Physics(player);
 		playerPhysics.width = 32;
-		playerPhysics.height = 32;
+		playerPhysics.height = 40;
 		playerPhysics.offsetX = 16;
-		playerPhysics.offsetY = 32;
+		playerPhysics.offsetY = 26;
+		new KeyboardInput(player);
+		const playerJustMovement = new JustMovement(player);
+		playerJustMovement.speed = 100;
+		const playerAnimation = new Animation(player);
+		playerAnimation.frontWalk = "dude-front-walk";
+		playerAnimation.backWalk = "dude-back-walk";
+		playerAnimation.leftWalk = "dude-left-walk";
+		playerAnimation.rightWalk = "dude-right-walk";
+
+		// enemy (components)
+		const enemyPhysics = new Physics(enemy);
+		enemyPhysics.width = 32;
+		enemyPhysics.height = 32;
+		enemyPhysics.offsetX = 16;
+		enemyPhysics.offsetY = 32;
 
 		this.floor_1 = floor_1;
 		this.wall_1 = wall_1;
 		this.player = player;
+		this.enemy = enemy;
 		this.cave_test_map_1 = cave_test_map_1;
+		this.enemyTeam = enemyTeam;
 
 		this.events.emit("scene-awake");
 	}
@@ -56,20 +80,42 @@ export default class Level extends Phaser.Scene {
 	private floor_1!: Phaser.Tilemaps.TilemapLayer;
 	private wall_1!: Phaser.Tilemaps.TilemapLayer;
 	public player!: Phaser.GameObjects.Sprite;
+	private enemy!: Phaser.GameObjects.Sprite;
+	private enemyTeam!: Phaser.GameObjects.Sprite[];
 
 	/* START-USER-CODE */
 	public platformer_fun!: Phaser.Tilemaps.Tilemap
 	// Write your code here
 
-	preload()
-	{
-		//this.load.image("FufuSuperDino", '../../static/assets/FufuSuperDino.png')
-	}
-
 	create() {
 
 		this.editorCreate();
-		this.player.play('swarm-right-idle')
+		this.player.play('dude-front-idle')
+
+		const playerKeyboardInput = KeyboardInput.getComponent(this.player)
+		const playerMove = JustMovement.getComponent(this.player)
+		const playerAnims = Animation.getComponent(this.player)
+
+		playerKeyboardInput.executeLeft = () => {
+			playerMove.moveLeft()
+			playerAnims.playLeft()
+		}
+		playerKeyboardInput.executeRight = () => {
+			playerMove.moveRight()
+			playerAnims.playRight()
+		}
+		playerKeyboardInput.executeUp = () => {
+			playerMove.moveUp()
+			playerAnims.playBack()
+		}
+		playerKeyboardInput.executeDown = () => {
+			playerMove.moveDown()
+			playerAnims.playFront()
+		}
+		playerKeyboardInput.executeKeyUp = () => {
+			playerMove.stayStill()
+			playerAnims.playIdleFromWalk()
+		}
 
 		this.physics.add.collider(this.player, this.wall_1);
 	}
