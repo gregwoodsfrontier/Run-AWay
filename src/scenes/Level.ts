7dc7bce7d2380f2e4e4b2268a8d106a41a0d9d7e
Richard <iwannaplaygamesjp@gmail.com>
2022-04-15 11:20:06 +0800

    fix things

--- src/scenes/Level.ts
+++ src/scenes/Level.ts
@@ -20,6 +20,13 @@ export default class Level extends Phaser.Scene {
 
 	editorCreate(): void {
 
+		// cave_test_map_1
+		const cave_test_map_1 = this.add.tilemap("cave-test-map-1");
+		cave_test_map_1.addTilesetImage("Gamdev jam cate tiles test 1", "cave-test-tileset-1");
+
+		// cave
+		const cave = cave_test_map_1.createLayer("Tile Layer 1", ["Gamdev jam cate tiles test 1"], 0, 0);
+
 		// player
 		const player = this.add.sprite(180, 283, "your-dude", 2);
 
@@ -27,11 +34,14 @@ export default class Level extends Phaser.Scene {
 		new Movement(player);
 		new Physics(player);
 
+		this.cave = cave;
 		this.player = player;
+		this.cave_test_map_1 = cave_test_map_1;
 
 		this.events.emit("scene-awake");
 	}
 
+	public cave!: Phaser.Tilemaps.TilemapLayer;
 	public player!: Phaser.GameObjects.Sprite;
 
 	/* START-USER-CODE */
--- src/scenes/Preload.ts
+++ src/scenes/Preload.ts
@@ -5,6 +5,8 @@
 
 import Phaser from "phaser";
 import PreloadText from "../components/PreloadText";
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
 
 export default class Preload extends Phaser.Scene {
 
@@ -19,15 +21,15 @@ export default class Preload extends Phaser.Scene {
 	editorCreate(): void {
 
 		// guapen
-		const guapen = this.add.image(400, 219, "guapen");
+		const guapen = this.add.image(160, 320, "guapen");
 		guapen.scaleX = 0.5915891440784282;
 		guapen.scaleY = 0.5915891440784282;
 
 		// progress
-		const progress = this.add.text(400, 349, "", {});
+		const progress = this.add.text(160, 475, "", {});
 		progress.setOrigin(0.5, 0.5);
 		progress.text = "0%";
-		progress.setStyle({"fontSize":"30px"});
+		progress.setStyle({ "fontSize": "30px" });
 
 		// progress (components)
 		new PreloadText(progress);
