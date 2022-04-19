9cbf51ef55ba9440c21d8c408b19e54d136b0ca2
Cabley123 <Creativeframemedia@gmail.com>
2022-04-19 10:42:57 +1000

    Title Scene Update
    Finished placement of Title.scene
    
    Imported Start Anim and Start Level Anim.

--- src/scenes/Title.ts
+++ src/scenes/Title.ts
@@ -20,18 +20,35 @@ export default class Title extends Phaser.Scene {
 	editorCreate(): void {
 
 		// image
-		this.add.image(160, 163, "gamedevjs");
+		this.add.image(-177, 162, "gamedevjs");
 
 		// text
-		const text = this.add.text(160, 300, "", {});
+		const text = this.add.text(-177, 300, "", {});
 		text.setOrigin(0.5, 0.5);
 		text.text = "Gamedev JS Game Jam\n2022";
 		text.setStyle({ "align": "center", "fontFamily": "Arial", "fontSize": "2em" });
 
 		// startGame
-		const startGame = this.add.text(54, 396, "", {});
+		const startGame = this.add.text(-283, 396, "", {});
 		startGame.text = "Start Game!";
 		startGame.setStyle({ "fontSize": "32px" });
+
+		// start_Anim1
+		this.add.image(160, 320, "Start Anim1");
+
+		// logo_v2
+		const logo_v2 = this.add.image(160, 142, "Logo v2");
+		logo_v2.scaleX = 0.7061279801116807;
+		logo_v2.scaleY = 0.7061279801116807;
+
+		// start_Button
+		this.add.image(160, 191, "Start Button");
+
+		// volume_Button
+		this.add.image(160, 255, "Volume Button");
+
+		// about_Button
+		this.add.image(160, 223, "About Button");
 
 		this.events.emit("scene-awake");
 	}
