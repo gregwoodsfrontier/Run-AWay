ca93522137de60a7d2d80bdc25164a95e7d92a52
Mr_Gumppy <sherifbasosy@yahoo.com>
2022-04-21 14:26:52 +0200

    Blasts Trap

---
+++ src/prefabs/BlastsTrap.ts
@@ -0,0 +1,85 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+import Phaser, { Time } from "phaser";
+import Blasts from "./Blasts";
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class BlastsTrap extends Phaser.GameObjects.Sprite {
+
+    public player: Phaser.GameObjects.Sprite;
+    public wall : Phaser.Tilemaps.TilemapLayer;
+    public timer: Phaser.Time.TimerEvent;
+
+	constructor(scene: Phaser.Scene, x?: number, y?: number, texture?: string, frame?: number | string) {
+		super(scene, x ?? 0, y ?? 0, texture || "raw-break-interact" , frame ?? 96);
+
+		/* START-USER-CTR-CODE */
+        this.scene.physics.add.existing(this)
+        this.StartTimer()
+		/* END-USER-CTR-CODE */
+	}
+    
+    StartTimer(){
+            this.timer = this.scene.time.addEvent({
+                delay: 3000,                // ms
+                callback: () =>{
+                    this.CreateBlasts()
+                },
+                loop: -1
+            });
+
+    }
+
+  CreateBlasts(){
+      var distanceX = Math.abs(this.player.x) - Math.abs(this.x)
+      var distanceY = Math.abs(this.player.y) - Math.abs(this.y)
+
+      this.play("trap-explode");
+
+      if(distanceX <= 150 && distanceY <= 150){
+          const blast = new Blasts(this.scene , this.x , this.y)
+          this.scene.add.existing(blast)
+          const body = blast.body as Phaser.Physics.Arcade.Body;
+          body.setVelocity(70,0);
+          //
+
+          const blast1 = new Blasts(this.scene , this.x , this.y)
+          this.scene.add.existing(blast1)
+          blast1.angle = 90;
+          const body1 = blast1.body as Phaser.Physics.Arcade.Body;
+          body1.setVelocity(0,70);
+  
+          //
+
+          const blast2 = new Blasts(this.scene , this.x , this.y)
+          this.scene.add.existing(blast2)
+          blast2.angle = 180;
+          const body2 = blast2.body as Phaser.Physics.Arcade.Body;
+          body2.setVelocity(-70 , 0);
+          
+          //
+
+          const blast3 = new Blasts(this.scene , this.x , this.y)
+          this.scene.add.existing(blast3)
+          blast3.angle = -90;
+          const body3 = blast3.body as Phaser.Physics.Arcade.Body;
+          body3.setVelocity(0,-70);
+
+         
+
+          const blasts = [blast, blast1 , blast2 , blast3]
+        this.scene.physics.add.collider(blasts , this.wall , blast.DestroyBlasts)
+        /* Sets the player's damage
+        this.scene.physics.add.collider(this.player , blasts , blast.Damageplayer)
+        */
+      }
+      else{
+          this.play("trap-idle")
+      }
+    
+  }
+
+}
