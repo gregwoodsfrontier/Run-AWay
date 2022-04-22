c1a06857b26bdbe27564d9f3e883748ed1210bb6
Richard <iwannaplaygamesjp@gmail.com>
2022-04-22 18:15:12 +0800

    make all buttons inc lass scope

--- src/scenes/PauseMenu.ts
+++ src/scenes/PauseMenu.ts
@@ -29,22 +29,34 @@ export default class PauseMenu extends Phaser.Scene {
 		this.add.image(288, 32, "Menu Button Pressed");
 
 		// start_Button
-		this.add.image(160, 255, "Start Button");
+		const start_Button = this.add.image(160, 255, "Start Button");
 
 		// restart_Button
-		this.add.image(160, 287, "Restart Button");
+		const restart_Button = this.add.image(160, 287, "Restart Button");
 
 		// volume_Button
-		this.add.image(160, 319, "Volume Button");
+		const volume_Button = this.add.image(160, 319, "Volume Button");
 
 		// about_Button
-		this.add.image(160, 383, "About Button");
+		const about_Button = this.add.image(160, 383, "About Button");
 
 		// mainMenuButton
-		this.add.image(160, 351, "MainMenuButton");
+		const mainMenuButton = this.add.image(160, 351, "MainMenuButton");
+
+		this.start_Button = start_Button;
+		this.restart_Button = restart_Button;
+		this.volume_Button = volume_Button;
+		this.about_Button = about_Button;
+		this.mainMenuButton = mainMenuButton;
 
 		this.events.emit("scene-awake");
 	}
+
+	private start_Button!: Phaser.GameObjects.Image;
+	private restart_Button!: Phaser.GameObjects.Image;
+	private volume_Button!: Phaser.GameObjects.Image;
+	private about_Button!: Phaser.GameObjects.Image;
+	private mainMenuButton!: Phaser.GameObjects.Image;
 
 	/* START-USER-CODE */
 
