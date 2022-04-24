4038270b184f38a4f08c2a109186ad03d6f4edda
Cabley123 <Creativeframemedia@gmail.com>
2022-04-24 19:18:56 +1000

    added Win end scene

---
+++ src/scenes/YouSurvived.ts
@@ -0,0 +1,67 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+import Phaser from "phaser";
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class YouSurvived extends Phaser.Scene {
+
+	constructor() {
+		super("YouSurvived");
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	editorCreate(): void {
+
+		// 70per_Black_BG
+		this.add.image(160, 320, "70per-Black-BG");
+
+		// byTeam
+		this.add.image(65, 562, "ByTeam");
+
+		// logo_v2
+		const logo_v2 = this.add.image(160, 320, "Logo v2");
+		logo_v2.scaleX = 0.885327816705385;
+		logo_v2.scaleY = 0.885327816705385;
+
+		// youSurvived
+		const youSurvived = this.add.image(160, 180, "YouSurvived");
+		youSurvived.scaleX = 3.5937905673970443;
+		youSurvived.scaleY = 3.5937905673970443;
+
+		// mainMenu_Button_Red
+		this.add.image(160, 386, "MainMenu-Button-Red");
+
+		// mainMenu_Button_Red_Selected
+		this.add.image(160, 386, "MainMenu-Button-Red-Selected");
+
+		// newGame_Button_Red
+		this.add.image(160, 420, "NewGame-Button-Red");
+
+		// newGame_Button_Red_Selected
+		this.add.image(160, 419, "NewGame-Button-Red-Selected");
+
+		this.events.emit("scene-awake");
+	}
+
+	/* START-USER-CODE */
+
+	// Write your code here
+
+	create() {
+
+		this.editorCreate();
+	}
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
