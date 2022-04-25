b3aadbbac051c12369dc5061b9841b8eb18a697e
Richard <iwannaplaygamesjp@gmail.com>
2022-04-25 12:33:18 +0800

    added block prefab empty

---
+++ src/prefabs/Block.ts
@@ -0,0 +1,29 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+import Phaser from "phaser";
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class Block extends Phaser.GameObjects.Image {
+
+	constructor(scene: Phaser.Scene, x?: number, y?: number, texture?: string, frame?: number | string) {
+		super(scene, x ?? 0, y ?? 0, texture || "raw-break-interact", frame ?? 0);
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/* START-USER-CODE */
+
+	// Write your code here.
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
