d4995279d0e687b32efed3f3af39dd8adf0d8aaf
Richard <iwannaplaygamesjp@gmail.com>
2022-05-11 08:43:33 +0800

    update base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/gamedevjs-project/',
+  base: '/Run-AWay/',
   publicDir: "static",
   resolve:{
     alias:{
